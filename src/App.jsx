7e13d8752a1a7ec1d3710c0d692ef459cf12a67d
Ha Do <meongoccute@gmail.com>
2024-10-28 22:37:10 +0800

    cm

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import HeroSection from './Components/HeroSection';
+import FeatureSection from './Components/FeatureSection';
+import Workflow from './Components/Workflow';
+import Pricing from './Components/Pricing';
+import Testimonials from './Components/Testimonials';
+import Footer from './Components/Footer';
+
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection/>
+      </div>
+      <FeatureSection/>
+      <Workflow/>
+      <Pricing/>
+      <Testimonials/>
+      <Footer/>
+      
+    </>
+  )
+}
+
+export default App
